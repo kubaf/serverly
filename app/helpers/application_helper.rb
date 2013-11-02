1d6c553e10ea33c0a71084ae73e44bde991acb6b
Kuba Fietkiewicz <kuba.fietkiewicz@gmail.com>
2013-11-02 13:40:19 -0700

    add options for select helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def object_options_for_select(object)
+    objects = object.to_s.capitalize.constantize.all
+    objects_array = objects.map { |object| [object.name, object.id] }
+    options_for_select(objects_array)
+  end
 end
